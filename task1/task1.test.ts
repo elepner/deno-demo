1bfcf82c1afa1cf3aec66cd4467a6b52594d1b4d
Eduard Lepner <eduard@invarion.com>
2024-12-03 10:27:15 +0100

    working debugger

--- task1/task1.test.ts
+++ task1/task1.test.ts
@@ -1,6 +1,18 @@
 import { expect } from "jsr:@std/expect";
 import solution from './task1.ts';
+
+
 Deno.test("should return diffs", () => {
+  debugger;
   const result = solution([1, 2, 5, 7]);
+
+  expect({ result }).toMatchObject({ result: [1, 3, 2] })
+})
+
+
+Deno.test("should return diffs 2", () => {
+  debugger;
+  const result = solution([1, 2, 5, 7]);
+
   expect({ result }).toMatchObject({ result: [1, 3, 2] })
 })
