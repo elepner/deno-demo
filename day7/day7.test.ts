d11ad40124731514af0c0cbc83ad51f4c1f240ee
Eduard Lepner <eduard@invarion.com>
2024-12-07 12:02:21 +0100

    solve with brute force

--- day7/day7.test.ts
+++ day7/day7.test.ts
@@ -35,7 +35,55 @@ function evaluate(target: number, currentAggregate: number, rest: number[], ops:
   return null;
 }
 
+function* generateAllOps(seq: number[]) {
 
+  const count = 2 ** (seq.length - 1);
+
+  for (let i = 0; i < count; i++) {
+
+    const opArray: Op[] = [];
+
+    for (let j = 0; j < seq.length - 1; j++) {
+
+      const mask = 1 << j;
+      const op = (i & mask);
+      if (op === 0) {
+        opArray.push('+');
+      } else {
+        opArray.push('*');
+      }
+    }
+
+    yield opArray;
+  }
+}
+
+function solve(nums: number[], ops: Op[]) {
+  let [current, ...rest] = nums;
+  for (let i = 0; i < rest.length; i++) {
+    const currentOp = ops[i];
+
+    const fn = opsMap[currentOp];
+    if (!fn) {
+      debugger
+    }
+
+    current = opsMap[currentOp](current, rest[i])
+  }
+
+  return current;
+}
+
+function bruteForce(target: number, vals: number[]) {
+  for (const ops of generateAllOps(vals)) {
+    const result = solve(vals, ops);
+    if (result === target) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 function checkSeq(target: number, vals: number[]) {
   const [nextNum, ...nextRest] = vals;
@@ -46,16 +94,27 @@ function checkSeq(target: number, vals: number[]) {
 
 function solvePt1(input: string) {
   return parseInput(input).map(([target, seq]) => {
-    return [target, checkSeq(target, seq)] as const
+    return [target, bruteForce(target, seq)] as const
   }).filter(([, isOk]) => isOk).reduce((acc, [target]) => {
     return acc + target
   }, 0);
 }
 
+
 function parseInput(input: string) {
   return input.trim().split(EOL).map((str) => {
     const [target, seq] = str.split(':');
-    return [Number(target), seq.split(' ').map(Number)] as const
+    return [Number(target), seq.trim().split(' ').map(x => {
+      const result = Number.parseInt(x);
+      if (result == 0) {
+        debugger
+      }
+
+      if (Number.isNaN(result)) {
+        throw new Error('is nan!')
+      }
+      return result
+    })] as const
   })
 }
 
@@ -75,7 +134,13 @@ const testCases: [number, number[], boolean][] = [
 Deno.test('should pass all cases', () => {
   for (const [t, seq, result] of testCases) {
     const res = checkSeq(t, seq);
+    const bf = bruteForce(t, seq);
+
+
     expect(res, `seq ${seq.join(',')} is ${result ? 'correct' : 'incorrect'}`).toBe(result);
+
+    expect(bf, `brute force is different for seq ${seq.join(',')}`).toBe(res);
+
   }
 });
 const sample = `
@@ -97,7 +162,21 @@ Deno.test('shoould solve sample', () => {
 Deno.test('should solve pt1', async () => {
   const content = await Deno.readTextFile('./day7/input.txt');
   const result = solvePt1(content);
+
   expect(result).toBeGreaterThan(3312209544174);
 
+  console.log(result);
+});
+
+Deno.test('check brute force', async () => {
+  const input = parseInput(await Deno.readTextFile('./day7/input.txt'));
+
+  for (const [target, seq] of input) {
+    const isOk = checkSeq(target, seq);
+
+    const bf = bruteForce(target, seq);
+
+    expect(isOk, `seq does not match ${seq.join(',')}`).toBe(bf);
+  }
 
 })
